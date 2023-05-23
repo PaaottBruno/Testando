5df22eb34c329aed1b098190fdddaaaf7aa6e9ba
PaaottBruno <132005298+PaaottBruno@users.noreply.github.com>
2023-05-23 13:40:44 -0400

    graficandooooo

--- bla.py
+++ bla.py
@@ -3,5 +3,10 @@ b = float(input("Digite um numero: "))
 c = float(input("Digite um numero: "))
 
 soma = (a+b+c)/3
-
+print(a,"+",b,"+",c,"=",a+b+c)
+print()
+print(a+b+c,"|  3  ")
+print(" "*4+"------")
+print(" "*4,soma)
+print()
 print("Sua media é: %.0f"%soma)
