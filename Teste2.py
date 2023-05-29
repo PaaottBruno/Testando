c78e269e7fe2c91f64fdf99fe87a5c465677a1a3
PaaottBruno <132005298+PaaottBruno@users.noreply.github.com>
2023-05-29 17:30:15 -0400

    removed

--- Teste2.py
+++
@@ -1,8 +0,0 @@
-
-num1 = int(input("Digite um numero: "))
-num2 = int(input("Digite um numero : "))
-resto = num1 % num2   # calculo do resto
-print("O resto é %d"%resto)
-
-if resto == 0:   # se for 0 e par
-    print("O resto é par")
