ef54d7bc8950416b5ba65e50c3b2a2394cfc95e1
PaaottBruno <132005298+PaaottBruno@users.noreply.github.com>
2023-06-20 16:51:59 -0400

    terminar

---
+++ teste_class.py
@@ -0,0 +1,9 @@
+class Peca:
+
+    def __init__(self, frente, tras, comer, promocao, lado):
+        self.frente = frente
+        self.tras = tras
+        self.comer = comer
+        self.promocao = promocao
+        self.lado = lado
+        
