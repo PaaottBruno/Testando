429615bc4db9e71517f6be8e6ad05b5ae7ef198f
Javador48 <paaott08@gmail.com>
2023-06-29 21:44:26 -0300

    update

--- testes.py
+++ testes.py
@@ -1,17 +1,37 @@
-arm = input(">>> ")
-c = arm.isdigit()
-if c:
-    print("correto")
-if not c:
-    print("erro")
+# arm = input(">>> ")
+# c = arm.isdigit()
+# if c:
+#     print("correto")
+# if not c:
+#     print("erro")
+
+# import getpass
+# import os
+
+# import pwinput  # modulo que esconde sua senha com ***, para utilizar deve-se digitar no terminal
+#                 # pip install pwinput, e dar um enter pronto estara intalando
+
+# a = pwinput.pwinput("Digite sua senha: ")  # condição para usar o pwinput, deve usar pwinput.pwinput
+# print(f"Sua senha digita foi: {a}")
+
+# user = os.getlogin()
+
+# print(f"Meu nome é {user}")
+
+# import getpass
+
+# user = getpass.getpass()
 
 import getpass
-
-nome = input("Digite seu nome: ")
+  
 user = getpass.getuser()
+<<<<<<< Updated upstream
 
 print(f"Meu nome é {user}")
 
 import pynput
 
 a = pynput("Digite: ")
+=======
+print("\n\n\n",user,"\n\n\n")
+>>>>>>> Stashed changes
